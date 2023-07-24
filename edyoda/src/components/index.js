7c85c4741584e8e88e136c7e05669fbc2e36cc68
Akshay Baiplawat <106819799+akshay-baiplawat@users.noreply.github.com>
2023-07-24 13:57:55 +0530

    Delete edyoda directory
    old

--- edyoda/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as NavBar } from './NavBar';
-export { default as Body } from './Body';
-export { default as MainLockup } from './MainLockup';
-export { default as Form } from './Form';
-export { default as PriceClick } from './PriceClick';
